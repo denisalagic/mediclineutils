22da68a68c640f2e82518fad40fc1eec74d92808
dalagic <denisalagichr@gmail.com>
2025-08-03 18:26:16 +0200

    excel colors

--- lib/office_utils/utils/color_util.dart
+++ lib/office_utils/utils/color_util.dart
@@ -9,36 +9,32 @@ class ColorUtil {
       String themeId,
       List<SSColorSchemes> colorSchemes
       ) {
-    // First, try indexed color
     if (bgClrIndex.isNotEmpty) {
       try {
         int index = int.parse(bgClrIndex);
         if (index < 64) {
-          return IndexedColor().colors[index]; // always #xxxxxx
-        } else if (index == 64) {
-          // Now try theme
+          return IndexedColor().colors[index];
+        } else if (index == 64 && themeId.isNotEmpty) {
+          themeId = themeId.toLowerCase().replaceAll("a:", "").trim();
+
           var clrScheme = colorSchemes.firstWhereOrNull((clrSch) => clrSch.id == themeId);
           if (clrScheme != null) {
             String? hex = clrScheme.srgbClr.isNotEmpty
                 ? clrScheme.srgbClr
                 : clrScheme.sysClrLast;
-
             if (hex.isNotEmpty) {
-              // Remove any extra '#' just in case
-              hex = hex.replaceAll("#", "").toUpperCase();
-              // Take last 6 digits (Excel colors sometimes include alpha)
+              hex = hex.replaceAll("#", "");
               if (hex.length > 6) hex = hex.substring(hex.length - 6);
               return "#$hex";
             }
           }
-          return "#FFFFFF"; // fallback to white
         }
       } catch (_) {
-        return "#FFFFFF"; // fallback
+        return "#FFFFFF";
       }
     }
-
-    return null;
+    return "#FFFFFF";
   }
+
 
 }
